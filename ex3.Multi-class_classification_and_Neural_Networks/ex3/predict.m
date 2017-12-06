523fe06ba1fe5801ff19f5d2e799c85f04dc54c5
Michael Luo <mluo@echoice.com.au>
2017-12-06 12:57:23 +1100

    update

--- ex3.Multi-class_classification_and_Neural_Networks/ex3/predict.m
+++ ex3.Multi-class_classification_and_Neural_Networks/ex3/predict.m
@@ -7,6 +7,7 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
+
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
@@ -22,7 +23,13 @@ p = zeros(size(X, 1), 1);
 %
 
 
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = [ones(size(z2, 1), 1) sigmoid(z2)];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
+[p_max, p] = max(a3, [], 2);
 
 
 
